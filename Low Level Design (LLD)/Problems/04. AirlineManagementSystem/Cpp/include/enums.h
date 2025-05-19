4b28a87137de6d45c9078a94b0f7bdd3a25577ff
Deepjyoti <deepjyoti0072@gmail.com>
2025-05-19 21:28:57 +0530

    Added LLD Problem

---
+++ Low Level Design (LLD)/Problems/04. AirlineManagementSystem/Cpp/include/enums.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum class SeatClass { ECONOMY, BUSINESS };
+enum class SeatStatus { AVAILABLE, BOOKED };
+enum class UserRole { PASSENGER, ADMIN };
+enum class BookingStatus { CONFIRMED, CANCELLED, PENDING };
+enum class PaymentStatus { SUCCESS, FAILED, PENDING };
