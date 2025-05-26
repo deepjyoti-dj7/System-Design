7feac58b1be03094b465075d996881d081aeb73f
Deepjyoti <deepjyoti0072@gmail.com>
2025-05-26 23:33:13 +0530

    Modified Order Class

--- Low Level Design (LLD)/Problems/05. FoodDeliverySystem/Java/models/Order.java
+++ Low Level Design (LLD)/Problems/05. FoodDeliverySystem/Java/models/Order.java
@@ -44,4 +44,8 @@ public class Order {
     public OrderStatus getStatus() {
         return status;
     }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
