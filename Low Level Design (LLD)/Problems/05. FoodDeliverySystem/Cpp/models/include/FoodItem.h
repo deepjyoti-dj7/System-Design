4fc84cd290f7e6d9e1d044b0ed431af0c94edb85
Deepjyoti <deepjyoti0072@gmail.com>
2025-06-02 22:30:42 +0530

    Added FoodItem class

--- Low Level Design (LLD)/Problems/05. FoodDeliverySystem/Cpp/models/include/FoodItem.h
+++ Low Level Design (LLD)/Problems/05. FoodDeliverySystem/Cpp/models/include/FoodItem.h
@@ -11,7 +11,7 @@ private:
 public:
     FoodItem(const std::string& id, const std::string& name, const double& price);
 
-    const std::string getId() const;
-    const std::string getName() const;
-    const std::string getPrice() const;
+    const std::string& getId() const;
+    const std::string& getName() const;
+    double getPrice() const;
 };
--- Low Level Design (LLD)/Problems/05. FoodDeliverySystem/Cpp/models/src/FoodItem.cpp
+++ Low Level Design (LLD)/Problems/05. FoodDeliverySystem/Cpp/models/src/FoodItem.cpp
@@ -3,14 +3,14 @@
 FoodItem::FoodItem(const std::string& id, const std::string& name, const double& price) 
 : id(id), name(name), price(price) {}
 
-const std::string FoodItem::getId() const {
+const std::string& FoodItem::getId() const {
     return id;
 }
 
-const std::string FoodItem::getName() const {
+const std::string& FoodItem::getName() const {
     return name;
 }
 
-const std::string FoodItem::getPrice() const {
+double FoodItem::getPrice() const {
     return price;
 }
