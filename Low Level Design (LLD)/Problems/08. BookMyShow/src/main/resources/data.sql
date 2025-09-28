948d8eb75496a78b7e260a8cb11a871b86d8fb5a
Deepjyoti <deepjyoti0072@gmail.com>
2025-09-28 23:41:06 +0530

    Added dummy data

---
+++ Low Level Design (LLD)/Problems/08. BookMyShow/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO movie (id, title, duration_minutes, language, description) VALUES (1, 'The Great Adventure', 120, 'English', 'Adventure movie');
+
+
+INSERT INTO theatre (id, name, city, address) VALUES (1, 'Cineplex Mall', 'Bangalore', 'MG Road');
+
+
+INSERT INTO screen (id, theatre_id, name, total_seats) VALUES (1, 1, 'Screen 1', 30);
