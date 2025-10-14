2b136ca442d97980014da63882e1a61c01b5bc48
Deepjyoti <deepjyoti0072@gmail.com>
2025-10-14 23:18:54 +0530

    Updated booking controller

--- Low Level Design (LLD)/Problems/08. BookMyShow/src/main/java/BookMyShow/BookMyShow/controller/user/BookingController.java
+++ Low Level Design (LLD)/Problems/08. BookMyShow/src/main/java/BookMyShow/BookMyShow/controller/user/BookingController.java
@@ -3,6 +3,7 @@ package BookMyShow.BookMyShow.controller.user;
 import BookMyShow.BookMyShow.dto.BookingDto.BookingRequest;
 import BookMyShow.BookMyShow.dto.BookingDto.BookingResponse;
 import BookMyShow.BookMyShow.service.BookingService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +16,14 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping
+    @Operation(summary = "Create a new booking", description = "Creates a new booking for a movie with the given details")
     public ResponseEntity<BookingResponse> createBooking(@RequestBody BookingRequest request) {
         BookingResponse response = bookingService.createBooking(request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/cancel/{bookingId}")
+    @Operation(summary = "Cancel booking", description = "Cancel booking for a movie with the given details")
     public ResponseEntity<String> cancelBooking(@PathVariable Long bookingId) {
         bookingService.cancelBooking(bookingId);
         return ResponseEntity.ok("Booking cancelled successfully");
