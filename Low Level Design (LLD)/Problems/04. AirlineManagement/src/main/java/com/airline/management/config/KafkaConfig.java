a18fbbae9d7744de30a17a8ce770ef5b894b3842
Deepjyoti <deepjyoti0072@gmail.com>
2025-11-11 22:36:30 +0530

    Added kafka config

---
+++ Low Level Design (LLD)/Problems/04. AirlineManagement/src/main/java/com/airline/management/config/KafkaConfig.java
@@ -0,0 +1,34 @@
+package com.airline.management.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic bookingCreatedTopic() {
+        return TopicBuilder.name("booking.created")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic paymentCompletedTopic() {
+        return TopicBuilder.name("payment.completed")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic paymentFailedTopic() {
+        return TopicBuilder.name("payment.failed")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
